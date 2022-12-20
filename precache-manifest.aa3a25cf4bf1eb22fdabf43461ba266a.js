e0f22f5f873bbcc131ec0bcd089ee630568cd2b9
TelevizorFisher <>
2022-12-20 20:59:49 +0200

    v1

---
+++ precache-manifest.aa3a25cf4bf1eb22fdabf43461ba266a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e0564ff6cc518a8a9ebd055945090f",
+    "url": "/SunriseLviv/index.html"
+  },
+  {
+    "revision": "150d865df230b15dc2c2",
+    "url": "/SunriseLviv/static/css/2.e22355b1.chunk.css"
+  },
+  {
+    "revision": "e981ccd0b94bd83bf3c7",
+    "url": "/SunriseLviv/static/css/main.478af2b0.chunk.css"
+  },
+  {
+    "revision": "150d865df230b15dc2c2",
+    "url": "/SunriseLviv/static/js/2.6eef9742.chunk.js"
+  },
+  {
+    "revision": "d15902dfb9dbd99aa1c04811621230b9",
+    "url": "/SunriseLviv/static/js/2.6eef9742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e981ccd0b94bd83bf3c7",
+    "url": "/SunriseLviv/static/js/main.967eb2dc.chunk.js"
+  },
+  {
+    "revision": "5d1cf675dcf26308ec2c",
+    "url": "/SunriseLviv/static/js/runtime-main.b30398dd.js"
+  },
+  {
+    "revision": "69fe1804e1ff8b2b8746d1c1a6770dbb",
+    "url": "/SunriseLviv/static/media/FonGreen.69fe1804.jpg"
+  },
+  {
+    "revision": "82adbbea2f026d460092f62a8ff9990b",
+    "url": "/SunriseLviv/static/media/M1.82adbbea.jpg"
+  },
+  {
+    "revision": "e813bbabbfdcc9413072088f42c975df",
+    "url": "/SunriseLviv/static/media/M2.e813bbab.jpg"
+  },
+  {
+    "revision": "c82a704756dd1cb5de411791b965b9e9",
+    "url": "/SunriseLviv/static/media/M3.c82a7047.jpg"
+  },
+  {
+    "revision": "c107ea59737e7cbdf9a859335ebd93bb",
+    "url": "/SunriseLviv/static/media/M4.c107ea59.jpg"
+  },
+  {
+    "revision": "489b9202b0c4f995d5356d08925dd467",
+    "url": "/SunriseLviv/static/media/M5.489b9202.jpg"
+  },
+  {
+    "revision": "e7917d22db33b448d48bc04d1171e8a4",
+    "url": "/SunriseLviv/static/media/M6.e7917d22.jpg"
+  },
+  {
+    "revision": "8daee08b697dad6c551c0466b1cee1d1",
+    "url": "/SunriseLviv/static/media/M7.8daee08b.jpg"
+  },
+  {
+    "revision": "34f7fa39ad23ce955999e6a35a8798a8",
+    "url": "/SunriseLviv/static/media/M8.34f7fa39.jpg"
+  },
+  {
+    "revision": "6f8a01c881b113355522924cdd6aab22",
+    "url": "/SunriseLviv/static/media/M9.6f8a01c8.jpg"
+  },
+  {
+    "revision": "feee9636fd4462078a6d9a21508a6929",
+    "url": "/SunriseLviv/static/media/Moon.feee9636.png"
+  },
+  {
+    "revision": "40c6f41e696affeb4b7e122a859fcc20",
+    "url": "/SunriseLviv/static/media/Sun.40c6f41e.png"
+  },
+  {
+    "revision": "846db24683afa37cafb0d4f5f441a62e",
+    "url": "/SunriseLviv/static/media/bgSun.846db246.jpg"
+  },
+  {
+    "revision": "bf88d825ae6befa1bba59ee3b73d4637",
+    "url": "/SunriseLviv/static/media/clock.bf88d825.png"
+  },
+  {
+    "revision": "2fccf5acac7dfd5c2f13dcd6db67c4b6",
+    "url": "/SunriseLviv/static/media/jalusi1.2fccf5ac.jpg"
+  },
+  {
+    "revision": "a4a29fafcc93b5a68e533644e43523ef",
+    "url": "/SunriseLviv/static/media/logoSun.a4a29faf.png"
+  },
+  {
+    "revision": "3ba98af4f392cdf79bcfcffeff61e202",
+    "url": "/SunriseLviv/static/media/nowimage.3ba98af4.jpg"
+  },
+  {
+    "revision": "6774a16f3abd996fdbbeac05a83230a0",
+    "url": "/SunriseLviv/static/media/phone.6774a16f.png"
+  },
+  {
+    "revision": "9bc78ebfda041f8c4ccf1c8b0744c8ba",
+    "url": "/SunriseLviv/static/media/social-2.9bc78ebf.png"
+  },
+  {
+    "revision": "817d585a7dd0775dbc9b41927bd043ca",
+    "url": "/SunriseLviv/static/media/social-3.817d585a.png"
+  },
+  {
+    "revision": "8e927f9ab7871cca539688eecb3c16c7",
+    "url": "/SunriseLviv/static/media/логотип-круг.8e927f9a.jpg"
+  },
+  {
+    "revision": "39191ef1d5462cfb0f06bd05406c1b4f",
+    "url": "/SunriseLviv/static/media/ролокасетаю.39191ef1.png"
+  }
+]);
